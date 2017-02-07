c5ec7b272db51c059e01e897cbe49bba6765edf4
Ayush Kohli <ayushk96@hotmail.com>
2017-02-06 20:55:03 -0600

    Adding jsonp=jsonp

--- getquote.js
+++ getquote.js
@@ -6,6 +6,7 @@ function getquote() {
             url : 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=parsequote',
             type: 'GET',
             dataType: 'jsonp',
+            jsonp: "jsonp",
             crossDomain: true,
             jsonpCallback: 'parsequote',
             
