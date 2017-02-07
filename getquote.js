6f4179c2caf850e171fd76f5a521585c596161fb
Ayush Kohli <ayushk96@hotmail.com>
2017-02-06 21:01:45 -0600

    Copy paste codepen code for testing

--- getquote.js
+++ getquote.js
@@ -2,38 +2,20 @@
 var quote;
 
 function getquote() {
-      $.ajax({
-            url : 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?',
-            type: 'GET',
-            dataType: 'jsonp',
-            jsonp: "jsonp",
-            crossDomain: true,
-            jsonpCallback: 'parsequote',
-            
-            
-            error : function(xhr,status,error){
-                console.log("WHOOPS");
-                console.log(xhr  + "XHR");
-                console.log(status + "STATUS");
-                console.log(error + "ERROR");
+        var forismaticURL = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?"
 
-            },
+  $.getJSON(forismaticURL, function(data) {
 
-            success : function(data,status,xhr){
-                
-                console.log(data);
-                console.log(status);
-                console.log(xhr);
-                quote=data;
-                console.log(Object.keys(quote));
-                $('#lol').text(quote['quoteText']);
-            },
-        });
-
-        function parsequote(data){
-            console.log("IN PARSEQUOTE");
-           // console.log(data);  
-        }
+    text.html(data.quoteText);
+    if (data.quoteAuthor) {
+      author.html(data.quoteAuthor);
+      author.attr("href", data.quoteLink);
+    } else {
+      author.removeAttr("href");
+      author.html("<strong>Anonymous</strong>");
+    }
+    tweetThis = data.quoteText + " By - " + data.quoteAuthor;
+  });
 
 };
 $(document).ready(function(){
