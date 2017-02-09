e215aef3b408b3eb286375197a208531d92da8e8
Ayush Kohli <ayushk96@hotmail.com>
2017-02-08 18:58:14 -0600

    Hacky solution to when json parsing fails

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 from flask import Flask
 import requests
+import json
 
 app = Flask(__name__)
+
+DEFAULT_QUOTE={u'quoteText': u'The awareness of our own strength makes us modest.', u'senderName': u'', u'senderLink': u'', u'quoteAuthor': u'Paul Cezanne', u'quoteLink': u'http://forismatic.com/en/0bef4d5e85/'}
 
 API = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json"
 
 def get_quote():
-    #try:
-    #print(requests.get(API).json())
-    return requests.get(API).json()
-    #finally:
-     #   return None
+    
+    try:
+
+        quote=requests.get(API).json()
+        quote=json.dumps(quote)
+        return quote
+    except Exception as e:
+        print e
+        return DEFAULT_QUOTE
+    #print quote
+
+
 
 
 @app.route('/')
