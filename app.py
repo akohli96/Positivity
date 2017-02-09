5f5729047645e1df165d4caa7605b28149311f78
Ayush Kohli <ayushk96@hotmail.com>
2017-02-08 18:19:14 -0600

    Got json printing on the webpage

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+API = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json"
 
+def get_quote():
+    #try:
+    #print(requests.get(API).json())
+    return requests.get(API).json()
+    #finally:
+     #   return None
+
+
+@app.route('/')
+def hello_world(quote=None):    
+    return str(get_quote())
+
+
+
+    
 if __name__ == '__main__':
+    
     app.run(debug=True)
