9c3a82d5be0bb2b93b636efde142407a87d70570
Ayush Kohli <ayushk96@hotmail.com>
2017-02-11 20:11:46 -0600

    Commented debugging info

--- static/click.js
+++ static/click.js
@@ -1,19 +1,26 @@
 document.onclick = function(event) {
-    // Compensate for IE<9's non-standard event model
-    // http://stackoverflow.com/questions/2621835/detect-click-on-browser
-    //if (event===undefined) event= window.event;
-    //var target= 'target' in event? event.target : event.srcElement;
 
-    console.log('clicked something in browser');
+    // http://stackoverflow.com/questions/2621835/detect-click-on-browser
+    //var target= 'target' in event? event.target : event.srcElement;
     //http://www.w3schools.com/xml/ajax_xmlhttprequest_send.asp
     //http://www.w3schools.com/xml/xml_http.asp
+    if (event === undefined) event = window.event;
+    var target = false;
+    if ('target' in event) {
+        true;
+    }
+
+
+
+    //console.log('clicked something in browser');
+
 
     var quoteRequest = new XMLHttpRequest();
     quoteRequest.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
 
-            console.log("It succeeded.")
-            document.getElementById("demo").innerHTML = quoteRequest.responseText;
+            // console.log("GET request to server to fetch quote succeeded.")
+            document.getElementById("quote").innerHTML = quoteRequest.responseText;
 
         }
     };
