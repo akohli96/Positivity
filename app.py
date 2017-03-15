9725ce9b700c861b3fa90a3f80402d95b2f4816e
Ayush Kohli <ayushk96@hotmail.com>
2017-03-14 22:31:36 -0500

    Documenting code

--- app.py
+++ app.py
@@ -1,52 +1,70 @@
 """
 Flask backend to display quote in browser
-#https://damyanon.net/flask-series-testing/
 """
-import json
+
+
 import logging
-from flask import Flask, render_template,jsonify
+from flask import Flask, render_template, jsonify
 import requests
+from constants import CONSTANTS
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 
-
-
-
 APP = Flask(__name__)
 
-DEFAULT_QUOTE = {u'quoteText': u'The awareness of our own strength makes us modest.',
-                 u'senderName': u'',
-                 u'senderLink': u'',
-                 u'quoteAuthor': u'Paul Cezanne',
-                 u'quoteLink': u'http://forismatic.com/en/0bef4d5e85/'}
-
-API = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json"
 
 def get_quote():
+    """
+    Fetches a quote from external API
+    Args:
+        None
+    Returns:
+        string
+
+    Example: quote=get_quote()
+
+    """
     logging.debug("In get quote :)")
     try:
 
-        quote = requests.get(API).json()
+        quote = requests.get(CONSTANTS['API']).json()
         return quote
     except Exception as e:
         logging.error(e)
-        return DEFAULT_QUOTE
-
-
+        return CONSTANTS['DEFAULT_QUOTE']
 
 
 @APP.route('/')
 def hello_world():
+    """
+    Fetches a quote from external API and renders index
+    Args:
+        None
+    Returns:
+        template
+
+    """
     positive_quote = get_quote()
     return render_template('index.html', quote=positive_quote)
 
-@APP.route('/getQuote', methods=['GET'])
-def getQuote():
-    logging.debug("In Getquote")
-    quote = get_quote()
-   
-    return jsonify(quote)
+
+@APP.route('/get_ajax_quote', methods=['GET'])
+def get_ajax_quote():
+    """
+    AJAX call GET method to render a quote using get_quote()
+    Args:
+        None
+    Returns:
+        json
+
+    """
+
+    logging.debug("In get_ajax_quote")
+    positive_quote = get_quote()
+
+    return jsonify(positive_quote)
+
 
 if __name__ == '__main__':
 
